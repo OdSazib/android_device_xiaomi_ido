a2e9e51dc9acdccf9afed15342f3f1f48dba7831
Shreesha Murthy <shreeshu10@gmail.com>
2016-08-08 04:10:37 +0530

    HACK: Data toggle fix
    Author: mweninge

--- device.mk
+++ device.mk
@@ -241,5 +241,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
     $(LOCAL_PATH)/wifi/WCNSS_wlan_dictionary.dat:system/etc/firmware/wlan/prima/WCNSS_wlan_dictionary.dat
 
+
 PRODUCT_PACKAGES += \
     wcnss_service
+
+# Data switch hack
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/netmgrd_control.sh:system/etc/netmgrd_control.sh
